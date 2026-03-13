ddbc3109c174d69002f449740ddece84bbd2cbc4
Andreas Schneider <schneider@mindtwo.de>
2026-03-13 14:59:54 +0100

    feat: add platform factory

--- tests/Fake/PlatformFactory.php
+++ tests/Fake/PlatformFactory.php
@@ -6,84 +6,33 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use mindtwo\LaravelPlatformManager\Models\Platform;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Platform>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<Platform>
  */
 class PlatformFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = Platform::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
+    public function definition(): array
     {
         return [
             'is_active' => true,
-            'name' => $this->faker->words(rand(1, 3), true),
-            'hostname' => $this->faker->domainName,
+            'hostname'  => $this->faker->domainName,
+            'settings'  => null,
         ];
     }
 
-    /**
-     * Indicate that the platform is hidden.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function main()
+    public function local(): static
     {
-        return $this->state(function (array $attributes) {
-            return [
-                'is_main' => 1,
-            ];
-        });
+        return $this->state(['hostname' => 'localhost']);
     }
 
-    /**
-     * Indicate that the platform is hidden.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function local()
+    public function inactive(): static
     {
-        return $this->state(function (array $attributes) {
-            return [
-                'hostname' => 'localhost',
-            ];
-        });
+        return $this->state(['is_active' => false]);
     }
 
-    /**
-     * Indicate that the platform is hidden.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function inactive()
+    public function active(): static
     {
-        return $this->state(function (array $attributes) {
-            return [
-                'is_active' => 0,
-            ];
-        });
-    }
-
-    /**
-     * Indicate that the platform is hidden.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function active()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'is_active' => 0,
-            ];
-        });
+        return $this->state(['is_active' => true]);
     }
 }
